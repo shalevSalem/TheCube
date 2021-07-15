94557773d399213148f8ab48cb95fd778241f649
shlaevSalem <shalevs345@gmail.com>
2021-07-16 01:32:27 +0300

    restart game with level at part 8

--- Assets/scripts/PlayerCollision.cs
+++ Assets/scripts/PlayerCollision.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovment movment;
-
+    
     void OnCollisionEnter(Collision collisionInfo) {
 
-        if (collisionInfo.collider.tag == "Obstacle") {
+        if (collisionInfo.collider.tag == "Obstacle") 
+        {
             movment.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
         }
+        
     }
 }
