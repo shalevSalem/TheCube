79df111006669e7c428d08a23186aeb7f1ff3674
shlomi <salemshlomi1@gmail.com>
2021-07-15 10:39:24 +0300

    implemnt obstacle in tags and also stop movment after hit an obstacle

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovment movment;
+
+    void OnCollisionEnter(Collision collisionInfo) {
+
+        if (collisionInfo.collider.tag == "Obstacle") {
+            movment.enabled = false;
+        }
+    }
+}
